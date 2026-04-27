9fedd0c44991a7e97576d388546edf39df487e7c
zkkaa <muhammadazkafakhri0@gmail.com>
2026-04-27 20:43:16 +0700

    new

---
+++ src/app/materi/barisan-deret/page.tsx
@@ -0,0 +1,10 @@
+// src/app/materi/barisan-deret/page.tsx
+
+export default function BarisanDeretPage() {
+  return (
+    <main>
+      <h1>Barisan dan Deret</h1>
+      {/* your content here */}
+    </main>
+  );
+}
